9c18db2d4c7de64581984bc508704d2e1284dbda
gortron <gordon.lanza@gmail.com>
2020-03-02 16:04:15 -0800

    Solved lesson 2 by setting up Express router() routes for item resource, and mounting the route to app() in server.js

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,22 @@
 import { Router } from 'express'
 
+const controller = (req, res) => {
+  res.send({message: "Hello from item.router"})
+}
+
 const router = Router()
+
+// /api/item
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+// /api/item/:id
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .delete(controller)
 
 export default router
